93e6bd9f719cf53bd37093680ecd8b8e2af3f608
PurvaK <purkar1993@gmail.com>
2024-01-30 20:06:29 +0530

    valid test cases

---
+++ spec/space_craft_spec.rb
@@ -0,0 +1,15 @@
+require 'space_craft'
+require 'pry'
+
+describe "SpaceCraft" do
+  context "for valid input" do 
+    it "should return position and direction" do
+      starting_point = [0,0,0]
+      initial_direction = 'N'
+      commands = ['f', 'r', 'u', 'b', 'l']
+      app = SpaceCraft.new(starting_point, initial_direction, commands)
+      expect(app.position).to eql [0,1,-1]
+      expect(app.direction).to eql "N"
+    end
+  end
+end
