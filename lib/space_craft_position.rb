699e44b4271809473a1041e698905651900ac17c
PurvaK <purkar1993@gmail.com>
2024-01-30 20:35:24 +0530

    move backward space craft method added

--- lib/space_craft_position.rb
+++ lib/space_craft_position.rb
@@ -16,6 +16,23 @@ module SpaceCraftPosition
     end
   end
 
+  def move_backward
+    case @direction
+    when "N"
+      @position[1] -= 1
+    when "S"
+      @position[1] += 1
+    when "E"
+      @position[0] -= 1
+    when "W"
+      @position[0] += 1
+    when "Up"
+      @position[2] -= 1
+    when "Down"
+      @position[2] += 1
+    end
+  end
+
   def turn_right
     directions = {
       "N" => "E",
